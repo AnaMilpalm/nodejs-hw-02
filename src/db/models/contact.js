e7df88b1517d6566d71811d3769af14c624cbac9
Anastacy Slyusar <ana.milpalm@gmail.com>
2025-01-30 21:01:07 +0100

    routers

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -1,33 +1,31 @@
 import { Schema, model } from 'mongoose';
 
-const contactsSchema = new Schema(
-  {
-    name: {
-      type: String,
-      required: true,
-    },
-    phoneNumber: {
-      type: String,
-      required: true,
-    },
-    email: {
-      type: String,
-      required: false,
-    },
-    isFavourite: {
-      type: Boolean,
-      default: false,
-    },
-    contactType: {
-      type: String,
-      enum: ['work', 'home', 'personal'],
-      required: true,
-      default: 'personal',
-    },
+const contactsSchema = new Schema({
+  name: {
+    type: String,
+    required: true,
   },
-  {
-    timestamps: true,
+
+  email: {
+    type: String,
+    required: false,
   },
-);
+  age: {
+    type: String,
+    required: true,
+  },
+  gender: {
+    type: String,
+    required: false,
+  },
+  avgMark: {
+    type: String,
+    default: false,
+  },
+  onDuty: {
+    type: Boolean,
+    required: true,
+  },
+});
 
 export const ContactsCollection = model('contacts', contactsSchema);
