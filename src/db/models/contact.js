d5ae82e9fb6e7943ff86ebe426e5bcbeb7a22aba
Anastacy Slyusar <ana.milpalm@gmail.com>
2025-02-04 21:43:47 +0100

    model was changed

--- src/db/models/contact.js
+++ src/db/models/contact.js
@@ -1,24 +1,34 @@
 import { Schema, model } from 'mongoose';
 
-const contactsSchema = new Schema({
-  name: {
-    type: String,
-    required: true,
+const contactsSchema = new Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+    },
+    phoneNumber: {
+      type: String,
+      required: true,
+    },
+    email: {
+      type: String,
+      required: false,
+    },
+    isFavourite: {
+      type: Boolean,
+      required: false,
+      default: false,
+    },
+    contactType: {
+      type: String,
+      required: true,
+      enum: ['work', 'home', 'personal'],
+    },
   },
-
-  email: {
-    type: String,
-    required: false,
+  {
+    timestamps: true,
+    versionKey: false,
   },
-
-  gender: {
-    type: String,
-    required: false,
-  },
-  avgMark: {
-    type: String,
-    default: false,
-  },
-});
+);
 
 export const ContactsCollection = model('contacts', contactsSchema);
